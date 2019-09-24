11d61130e609352de7c580aa266ebe26ab080ef2
Your Name <you@example.com>
2019-09-24 17:58:43 +0000

    peer programmed with Nmehanma and Alex

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,16 @@
-// pair programming with Aiman and Najeam
-// delay printing each letter in string by 50
+// // pair programming with Aiman and Najeam
+// // delay printing each letter in string by 50
 
-const sentence = "hello there from lighthouse labs";
-let delay = 0;
+const sentence = "hello there from lighthouse labs\n";
+
+let wait = 0
+
 for (const char of sentence) {
-  delay += 50;
   setTimeout(() => {
-    process.stdout.write(char)
-  }, delay)
-}
+    if(sentence.indexOf(char) === (sentence.length - 1)){
+      process.stdout.write(char+'\n');
+    } else {
+      process.stdout.write(char)  
+    }
+  }, wait += 50);
+};
